3904ec46b2be0e2c86e3161e44ab86b239b2aa4b
shoulderhu <shoulderhu@gmail.com>
2020-07-15 11:33:08 +0800

    Add #448

---
+++ 448-find-all-numbers-disappeared-in-an-array.c
@@ -0,0 +1,42 @@
+/*int *findDisappearedNumbers(int *nums, int numsSize, int *returnSize) {
+    int *ret = (int *)malloc(numsSize * sizeof(int));
+    int tmp;
+
+    for (int i = 0; i < numsSize; ++i) {
+        while (nums[i] != nums[nums[i] - 1]) {
+            tmp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = tmp;
+        }
+    }
+
+    *returnSize = 0;
+    for (int i = 0; i < numsSize; ++i) {
+        if (nums[i] != i + 1) {
+            ret[(*returnSize)++] = i + 1;
+        }
+    }
+
+    return ret;
+}*/
+
+int *findDisappearedNumbers(int *nums, int numsSize, int *returnSize) {
+    int *ret = (int *)malloc(numsSize * sizeof(int));
+    int tmp;
+
+    for (int i = 0; i < numsSize; ++i) {
+        int idx = abs(nums[i]) - 1;
+        if (nums[idx] > 0) {
+            nums[idx] = nums[idx] * -1;
+        }
+    }
+
+    *returnSize = 0;
+    for (int i = 0; i < numsSize; ++i) {
+        if (nums[i] > 0) {
+            ret[(*returnSize)++] = i + 1;
+        }
+    }
+
+    return ret;
+}
